356cae6387f2efad098e798cc9332ba288639bed
Kurokawa018 <kurokawa.kazuma018@gmail.comm>
2023-12-17 19:16:17 +0900

    フォロー関係をFirebaseに保存

--- lib/models/main/profile_model.dart
+++ lib/models/main/profile_model.dart
@@ -12,6 +12,7 @@ import 'package:flutter_udemy_sns/constants/strings.dart';
 import 'package:image_picker/image_picker.dart';
 
 import '../../domain/firestore_user/firestore_user.dart';
+import '../../domain/following_token/following_token.dart';
 import '../main_model.dart';
 
 final profileProvider = ChangeNotifierProvider(
@@ -40,13 +41,28 @@ class ProfileModel extends ChangeNotifier{
     notifyListeners();
   }
 
-  void follow({required MainModel mainModel,required FirestoreUser passiveFirestoreUser}) {
+  Future<void> follow({required MainModel mainModel,required FirestoreUser passiveFirestoreUser }) async {
+    // settings
     mainModel.followingUids.add(passiveFirestoreUser.uid);
     notifyListeners();
+    final String tokenId = returnUuidV4();
+    final Timestamp now = Timestamp.now();
+    final FollowingToken followingToken = FollowingToken(createdAt: now,passiveUid: passiveFirestoreUser.uid,tokenId: tokenId);
+    final FirestoreUser activeUser = mainModel.firestoreUser;
+    // 自分がフォローした印
+    await FirebaseFirestore.instance.collection("users").doc(activeUser.uid).collection("tokens").doc(tokenId).set(followingToken.toJson());
   }
 
-  void unfollow({required MainModel mainModel,required FirestoreUser passiveFirestoreUser}) {
+  Future<void> unfollow({required MainModel mainModel,required FirestoreUser passiveFirestoreUser }) async {
     mainModel.followingUids.remove(passiveFirestoreUser.uid);
     notifyListeners();
+    // followしているTokenを取得する
+    final FirestoreUser activeUser = mainModel.firestoreUser;
+    // qshotというdataの塊の存在を存在を取得
+    final QuerySnapshot<Map<String, dynamic>>  qshot = await FirebaseFirestore.instance.collection("users").doc(activeUser.uid).collection("tokens").where("passiveUid",isEqualTo: passiveFirestoreUser.uid).get();
+    // 1個しか取得してないけど複数している扱い
+    final List<DocumentSnapshot<Map<String,dynamic>>> docs = qshot.docs;
+    final DocumentSnapshot<Map<String, dynamic>> token  = docs.first;
+    await token.reference.delete();
   }
 }
